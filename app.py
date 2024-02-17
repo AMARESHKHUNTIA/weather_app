37adc0fb0fd5c84fa73edc08e4f2d21e0eb0f6e2
AMARESHKHUNTIA <ak93khuntia@gmail.com>
2024-02-17 10:54:20 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask ,render_template, request
+import requests
+
+app=Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+
+@app.route('/submit',methods=['GET','POST'])
+def weather_report():
+    url='https://api.openweathermap.org/data/2.5/weather'
+    param={
+        'q':request.form.get('city'),
+        'appid':request.form.get('api_key'),
+        'units':request.form.get('unit')
+    }
+    response=requests.post(url=url,params=param)
+    data=response.json()
+    return f'data: {data}'
+
+if __name__==('__main__'):
+    app.run(host='0.0.0.0',port=5002)
+
+
